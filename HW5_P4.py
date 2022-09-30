2d7c70df912bdbd58ee0d82e3575dc583c72c275
wgeers <wgeers@purdue.edu>
2022-09-30 11:22:23 -0400

    Prob 4 eq

--- HW5_P4.py
+++ HW5_P4.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 rMag=np.linalg.norm(x)
-
+u=-G(m1+m2)
 vMag=np.linalg.norm(v)
+h=np.cross(x,v)
+hMag=np.abs(np.sqrt(h**2))
 
+a=((2/rMag)-(vMag**2)/u)**-1
 
+e=np.sqrt(1-(h**2/(u*a)))
+I=np.arccos()
