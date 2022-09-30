0687043e03f9bdd3e5112d288dc16c1f1ffc66b1
wgeers <wgeers@purdue.edu>
2022-09-30 13:04:20 -0400

    Q4 updates

--- HW5_P4.py
+++ HW5_P4.py
@@ -1,22 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-rMag=np.linalg.norm(x)
-u=-G(m1+m2)
-vMag=np.linalg.norm(v)
-h=np.cross(x,v)
-hMag=np.abs(np.sqrt(h**2))
 
-a=((2/rMag)-(vMag**2)/u)**-1
+def main():
+    rMag=np.linalg.norm(x)
+    u=-G(m1+m2)
+    vMag=np.linalg.norm(v)
+    h=np.cross(x,v)
+    hMag=np.abs(np.sqrt(h**2))
 
-e=np.sqrt(1-(h**2/(u*a)))
-I=np.arccos()
+    a=((2/rMag)-(vMag**2)/u)**-1
 
-plt.plot(t,a)
-plt.xlabel('Time(Myr)')
-plt.ylabel('semi-major axis(a)')
+    e=np.sqrt(1-(h**2/(u*a)))
+    I=np.arccos()
 
-plt.plot(t,I)
-plt.xlabel('Time(Myr)')
-plt.ylabel('Inclination')
+    plt.plot(t,a)
+    plt.xlabel('Time(Myr)')
+    plt.ylabel('semi-major axis(a)')
+
+    plt.plot(t,I)
+    plt.xlabel('Time(Myr)')
+    plt.ylabel('Inclination')
 
 
